41dfb999b3812f98ff36049fabba2a81b4081046
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-21 15:20:41 -0400

    Buttons handle clicks

--- src/components/barbutton/BarButton.js
+++ src/components/barbutton/BarButton.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 class BarButton extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    e.preventDefault()
+    this.props.onSelect(this.props.name)
+  }
 
 
   render(props) {
     return (
-      <h1>T</h1>
+      <div>
+        <h1 onClick={this.handleClick}>{this.props.name}</h1>
+      </div>
     );
   }
 }
