53533e15c0b08935bc779c37c5df3235f58cfa81
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-21 10:56:03 -0400

    Fetch dummy data with axios

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import Slide from '../slide/Slide'
+import axios from 'axios'
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      name: "who cares"
+      slides: []
     };
+  }
+
+  componentDidMount() {
+    axios.get('../../../data.json')
+      .then(response => {
+        console.log(response)
+      })
   }
 
   render() {
     return (
       <div>
-        <Slide name={this.state.name}/>
+        <Slide />
       </div>
     );
   }
