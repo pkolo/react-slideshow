451b356928665b4d8553eeb735c4063c488e6cc0
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-21 10:21:01 -0400

    Set up slide component

---
+++ src/components/slide/Slide.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Slide.css';
+
+class Slide extends Component {
+  render() {
+    return (
+      <h1>Hell world</h1>
+    );
+  }
+}
+
+export default Slide;
