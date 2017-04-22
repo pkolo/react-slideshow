64455f10f695bab630c87ed9cfdc18aab5ba5283
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-22 09:31:42 -0400

    Fix titles

--- src/components/slide/Slide.js
+++ src/components/slide/Slide.js
@@ -6,7 +6,7 @@ class Slide extends Component {
 
   render(props) {
     return (
-      <div className="Slide-title">Hell {this.props.slide.title}</div>
+      <div className="Slide-title">{this.props.slide.title}</div>
     );
   }
 }
