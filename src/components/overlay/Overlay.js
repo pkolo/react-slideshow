f1d00086348da4718861a312336e0237b196cba6
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-22 09:46:47 -0400

    Styling text overlay from JSON data

--- src/components/overlay/Overlay.js
+++ src/components/overlay/Overlay.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import './Overlay.css';
 
 class Overlay extends Component {
 
 
   render() {
     return (
-      <h1>test</h1>
+      <h1 style={this.props.style}>{this.props.text}</h1>
     );
   }
 }
