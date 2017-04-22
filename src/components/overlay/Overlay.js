c5a063bb6f7defc90bb23b611b4aaefacc59a5b7
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-22 09:37:39 -0400

    Create Overlay component

---
+++ src/components/overlay/Overlay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Overlay.css';
+
+class Overlay extends Component {
+
+
+  render() {
+    return (
+      <h1>test</h1>
+    );
+  }
+}
+
+export default Overlay;
